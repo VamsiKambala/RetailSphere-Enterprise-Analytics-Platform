fc9b4eed4f23a4c90604d5cc424dc71d5366ed58
VamsiKambala <captainmax414@gmail.com>
2026-07-31 19:53:20 +0530

    inserted data into stores_generator.py and created cities lookup table

--- 02_Raw_Data/generators/dimensions/stores_generator.py
+++ 02_Raw_Data/generators/dimensions/stores_generator.py
@@ -15,90 +15,70 @@ project_root=Path(__file__).resolve().parents[2]
 
 #Build the path
 
-regions_path=project_root/ "lookup" / "regions.csv"
+cities_path=project_root/ "lookup" / "cities.csv"
 #02_Raw_Data/lookup/regions.csv
 
 #Read the CSV
-regions=pd.read_csv(regions_path)
+cities=pd.read_csv(cities_path)
 
-#Columns to create
+market_weights = {
+    "Metro": 10,
+    "Urban": 4,
+    "Semi-Urban": 1
+    }
+cities["Weight"] = cities["MarketType"].map(market_weights)
 
-# StoreID	Python
-# StoreName	Python
-# StoreType	random.choice()
-# RegionID	random.choice(regions["RegionID"])
-# City	From regions.csv
-# OpeningDate	Random date
-# ManagerName	Faker
-# ContactNumber	Faker
-# Email	Python
-# IsActive	Random
-
-# 1-Generate StoreID
-
-i=1
-store_id=f"ST{i:04}" # Always make this number 4 digits long
-print(store_id)
-
-# 1.1 Pick a Region
-region_id=random.choice(regions["RegionID"])
-
-#1.2 Find the State for that Region
-state=regions.loc[
-    regions["RegionID"]==region_id,
-    "State"
-].values[0]
-print(state)
-#.loc means - Give me the rows where the condition is True."
-# regions["RegionID"] == region_id creates a Boolean condition (True/False) to identify 
-# the row where the selected RegionID matches. The .loc[row_condition, "State"] method 
-# then filters the DataFrame using that condition and returns only the value(s) from the 
-# "State" column for the matching row as a pandas Series. The .values attribute converts 
-# that Series into a NumPy array, and [0] extracts the first (and in this case, only) 
-# element from the array, giving the actual state name as a string.
-
-#1.3-Generate One Store
-
-for i in range(1,6):
-
-    store_id=f"ST{i:04}" # Always make this number 4 digits long
-    print(store_id)
-
-
-store_id = "ST0001"
-
-store_type = random.choice([
+stores=[]
+for i in range(1,200):
+    store_id=f"ST{i:04}"
+    city_id = random.choices(
+    population=cities["CityID"],
+    weights=cities["Weight"],
+    k=1
+    )[0]
+    city=cities.loc[
+    cities["CityID"]==city_id,
+    "City"
+    ].values[0]
+    store_name = f"RetailSphere {city} {store_id}"
+    manager_name = fake.name()
+    store_type = random.choices([
     "Physical Store",
     "Warehouse",
-    "Distribution Center"
-])
-
-region_id = random.choice(regions["RegionID"])
-
-state = regions.loc[
-    regions["RegionID"] == region_id,
-    "State"
-].values[0]
-
-manager_name = fake.name()
-
-contact_number = fake.phone_number()
-
-opening_date = fake.date_between(
+    "Distribution Center"],
+    weights=[90, 7, 3],
+    k=1
+    )[0]
+    contact_number = f"9{random.randint(100000000,999999999)}"
+    opening_date = fake.date_between(
     start_date="-10y",
     end_date="today"
+    )
+    is_active = random.choices(["Yes", "No"],weights=[98,2],k=1)[0]
+    email = f"{store_id.lower()}@retailsphere.com"
+
+
+    store={
+    "StoreID":store_id, # Always make this number 4 digits long
+    "CityID": city_id,
+    "ManagerName":manager_name,
+    "StoreName":store_name,
+    "StoreType":store_type,
+    "ContactNumber":contact_number,
+    "OpeningDate":opening_date,
+    "IsActive":is_active,
+    "Email":email
+    }
+    stores.append(store)
+
+stores_df = pd.DataFrame(stores)
+print(stores_df.head())
+
+output_path = project_root / "output" / "dimensions"
+output_path.mkdir(parents=True, exist_ok=True)
+
+stores_df.to_csv(
+    output_path / "stores.csv",
+    index=False
 )
 
-is_active = random.choice(["Yes", "No"])
-store_name = f"RetailSphere {state}"
-email = state.lower().replace(" ", "") + "@retailsphere.com"
-print(store_id)
-print(store_name)
-print(store_type)
-print(region_id)
-print(state)
-print(manager_name)
-print(contact_number)
-print(opening_date)
-print(email)
-print(is_active)
